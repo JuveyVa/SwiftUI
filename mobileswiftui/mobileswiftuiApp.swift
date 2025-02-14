5a6c34b066e4fd7abcbed224a573b64cc4ef3087
ITIT <ITIT@ULSAs-iMac.local>
2025-02-13 19:33:11 -0600

    se crea y se sube proyecto de iniciazion de swiftui

---
+++ mobileswiftui/mobileswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileswiftuiApp.swift
+//  mobileswiftui
+//
+//  Created by ITIT on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct mobileswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
